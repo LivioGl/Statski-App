2acadd0560d6e2c56f6bc5bb9b893ef4490cb49e
LivioGl <liviogalleano3@gmail.com>
2024-08-27 19:42:32 +0200

    Added 2021 season

--- app/src/main/java/com/example/statski/Athlete.kt
+++ app/src/main/java/com/example/statski/Athlete.kt
@@ -1,11 +1,10 @@
 package com.example.statski
-import java.io.Serializable
 
 data class Athlete(
     val name: String,
     val nation: String,
     val birth: Int
 
-    ): Serializable{
+    ){
     val performance_list = mutableListOf<Performance>()
 }
