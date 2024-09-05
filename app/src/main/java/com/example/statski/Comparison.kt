fbcf5403ee36a952f3a4bc5b59a2235e887ef5cb
LivioGl <liviogalleano3@gmail.com>
2024-09-05 22:33:22 +0200

    Started Comparison. Favourite Athletes needs to be concluded

--- app/src/main/java/com/example/statski/Comparison.kt
+++ app/src/main/java/com/example/statski/Comparison.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.statski.databinding.FragmentAthletesBinding
+import com.example.statski.databinding.FragmentComparisonBinding
 
 
 class Comparison : Fragment() {
+
+    private lateinit var binding : FragmentComparisonBinding
+    val viewModel_instance : AthletesViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comparison, container, false)
+        binding = FragmentComparisonBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
 
 }
