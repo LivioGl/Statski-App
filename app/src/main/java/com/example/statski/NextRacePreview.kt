c41e1b6ad39bc6724c291a7635be0bac401f4911
LivioGl <liviogalleano3@gmail.com>
2024-09-05 19:06:11 +0200

    Next race preview completed. Favourite Athletes needs to be concluded

--- app/src/main/java/com/example/statski/NextRacePreview.kt
+++ app/src/main/java/com/example/statski/NextRacePreview.kt
@@ -1,5 +1,6 @@
 package com.example.statski
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ class NextRacePreview : AppCompatActivity() {
     private lateinit var currentSlope : Slope
     private lateinit var winnersList: MutableList<Athlete>
 
+    @SuppressLint("DefaultLocale", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNextRacePreviewBinding.inflate(layoutInflater)
@@ -47,6 +49,38 @@ class NextRacePreview : AppCompatActivity() {
                 athlete.performance_list.count { it.place == currentSlope.location+"_m" && (it.position == "1" || it.position == "2" || it.position == "3") }
             }
 
+            // Calculating avg time delay of second and third place from winning athlete
+            val secondPlacePerformances = winnersList.flatMap {
+                athlete -> athlete.performance_list.filter { it.position == "2" && it.place == currentSlope.location+"_m"}
+                .map{it.total_time.replace("+", "").toDouble()}
+            }
+            val result_second_place = secondPlacePerformances.sum() / secondPlacePerformances.size
+            var avgDelaySecondPlace = String.format("+%.2f", result_second_place)
+
+
+            val thirdPlacePerformances = winnersList.flatMap {
+                    athlete -> athlete.performance_list.filter { it.position == "3" && it.place == currentSlope.location+"_m"}
+                .map{it.total_time.replace("+", "").toDouble()}
+            }
+            val result_third_place = thirdPlacePerformances.sum() / thirdPlacePerformances.size
+            var avgDelayThirdPlace = String.format("+%.2f", result_third_place)
+
+
+            // Calculating data about national teams
+            val historyPodiumPerformance = winnersList.flatMap {
+                    athlete -> athlete.performance_list.filter {
+                it.place == currentSlope.location+"_m" && (it.position == "1" || it.position == "2" || it.position == "3")
+            }.map{
+                athlete.nation
+            }
+            }
+            val nationPodiumsCount = historyPodiumPerformance.groupingBy { it }.eachCount()
+            val top3Nations : List<Map.Entry<String, Int>> = nationPodiumsCount.entries
+                .sortedByDescending { it.value }
+                .take(3)
+
+
+            // Calculating data and stats to fill TextViews
             if (athleteWithMostWins != null && athleteWithMostPodiums != null) {
                 val wins = athleteWithMostWins.performance_list.count{it.place == currentSlope.location+"_m" && it.position == "1"}
                 val podiums = athleteWithMostPodiums.performance_list.count{it.place == currentSlope.location+"_m" && (it.position == "1" || it.position == "2" || it.position == "3")}
@@ -54,11 +88,17 @@ class NextRacePreview : AppCompatActivity() {
                     binding.mostWinsAthlete.text = athleteWithMostWins.name+" is the king of "+currentSlope.name+". He has "+podiums+" podiums and "+wins+" victories on the "+currentSlope.name+": definitely the athlete to beat"
                     binding.mostPodiumsAthlete.text = "We expect a great performance from him"
                 } else {
-                    binding.mostWinsAthlete.text = athleteWithMostWins.name+", won "+wins.toString()+ " times on "+currentSlope.name
-                    binding.mostPodiumsAthlete.text = athleteWithMostPodiums.name+", who secured "+podiums.toString()+" podiums on the "+currentSlope.name+". He is always competitive here"
+                    binding.mostWinsAthlete.text = athleteWithMostWins.name+", won "+wins.toString()+ " times on the "+currentSlope.name
+                    binding.mostPodiumsAthlete.text = athleteWithMostPodiums.name+" secured "+podiums.toString()+" podiums on the "+currentSlope.name+". He is always competitive here"
                 }
             }
-        } else {
+
+            binding.avgDelaySecond.text =
+                "Based on previous editions, the average gap from first place is ${avgDelaySecondPlace}s for second place, while it is ${avgDelayThirdPlace}s for third place"
+
+            binding.bestTeamsList.text = "Based on previous editions, the team which had best performances is "+top3Nations[0].key+", with "+top3Nations[0].value+" podiums. Then also "+top3Nations[1].key+" and "+top3Nations[2].key+" which have "+top3Nations[1].value+ " and "+top3Nations[2].value+" podiums respectively."
+        }
+        else {
             val womenRaceJson = intent.getStringExtra("Women Race")
             val whoToWatchWomenJson = intent.getStringExtra("Who to watch women")
             val slopeJson = intent.getStringExtra("Women slope")
@@ -81,6 +121,37 @@ class NextRacePreview : AppCompatActivity() {
                 athlete.performance_list.count { it.place == currentSlope.location+"_f" && (it.position == "1" || it.position == "2" || it.position == "3") }
             }
 
+            // Calculating avg time delay of second and third place from winning athlete
+            val secondPlacePerformances = winnersList.flatMap {
+                    athlete -> athlete.performance_list.filter { it.position == "2" && it.place == currentSlope.location+"_f"}
+                .map{it.total_time.replace("+", "").toDouble()}
+            }
+            var result_second_place = secondPlacePerformances.sum() / secondPlacePerformances.size
+            var avgDelaySecondPlace = String.format("+%.2f", result_second_place)
+
+
+            val thirdPlacePerformances = winnersList.flatMap {
+                    athlete -> athlete.performance_list.filter { it.position == "3" && it.place == currentSlope.location+"_f"}
+                .map{it.total_time.replace("+", "").toDouble()}
+            }
+            var result_third_place = thirdPlacePerformances.sum() / thirdPlacePerformances.size
+            var avgDelayThirdPlace = String.format("+%.2f", result_third_place)
+
+
+            // Calculating data about national teams
+            val historyPodiumPerformance = winnersList.flatMap {
+                athlete -> athlete.performance_list.filter {
+                    it.place == currentSlope.location+"_f" && (it.position == "1" || it.position == "2" || it.position == "3")
+                }.map{
+                    athlete.nation
+                }
+            }
+            val nationPodiumsCount = historyPodiumPerformance.groupingBy { it }.eachCount()
+            val top3Nations : List<Map.Entry<String, Int>> = nationPodiumsCount.entries
+                .sortedByDescending { it.value }
+                .take(3)
+
+            // Calculating data and stats to fill TextViews
             if (athleteWithMostWins != null && athleteWithMostPodiums != null) {
                 val wins = athleteWithMostWins.performance_list.count{it.place == currentSlope.location+"_f" && it.position == "1"}
                 val podiums = athleteWithMostPodiums.performance_list.count{it.place == currentSlope.location+"_f" && (it.position == "1" || it.position == "2" || it.position == "3")}
@@ -88,10 +159,14 @@ class NextRacePreview : AppCompatActivity() {
                     binding.mostWinsAthlete.text = athleteWithMostWins.name+" is the queen of "+currentSlope.name+". She has "+podiums+" podiums and "+wins+" victories on the "+currentSlope.name+": definitely the athlete to beat"
                     binding.mostPodiumsAthlete.text = "We expect a great performance from her"
                 } else {
-                    binding.mostWinsAthlete.text = athleteWithMostWins.name+", won "+wins.toString()+ " times on "+currentSlope.name
+                    binding.mostWinsAthlete.text = athleteWithMostWins.name+", won "+wins.toString()+ " times on the "+currentSlope.name
                     binding.mostPodiumsAthlete.text = athleteWithMostPodiums.name+", who secured "+podiums.toString()+" podiums on the "+currentSlope.name+". She is always competitive here"
                 }
             }
+            binding.avgDelaySecond.text =
+                "Based on previous editions, the average gap from first place is ${avgDelaySecondPlace}s for second place, while it is ${avgDelayThirdPlace}s for third place"
+
+            binding.bestTeamsList.text = "Based on previous editions, the team which had best performances is "+top3Nations[0].key+", with "+top3Nations[0].value+" podiums. Then also "+top3Nations[1].key+" and "+top3Nations[2].key+" which have "+top3Nations[1].value+ " and "+top3Nations[2].value+" podiums respectively."
         }
 
         binding.mainTitle.text = currentRace.place+" "+currentRace.race_type
